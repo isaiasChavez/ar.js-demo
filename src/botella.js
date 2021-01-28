89d293d93132aeecf23741881f463a9b788bdd1c
Isaias Chavez Martinez <isaiaschavez.co@gmail.com>
2021-01-27 22:23:52 -0600

    .

--- src/botella.js
+++ src/botella.js
@@ -1,35 +1,53 @@
 AFRAME.registerComponent('botella', {
   schema: {
-    health: {type: 'int', default: 20},
-    alive: {type: 'boolean', default: true},
-    id: {type: 'int', default: 0}
+    health: { type: 'int', default: 20 },
+    alive: { type: 'boolean', default: true },
+    id: { type: 'int', default: 0 }
   },
 
-   tick: function () {
-    var entity = this.el;
-    
+  tick: function () {
+    var entity = this.el
+
     // console.log(this.data.health);
     // // if (this.data.health <= 0) {
-     // entity.parentNode.removeChild(entity);
-     // }
-    },
-    init: function () {
-     var data = this.data;
-     var el = this.el;
-     this.el.addEventListener( 'click', () => {
-      console.log("click");
-     })
-     
-     console.log("this.el.object3D",this.el.object3D );
-     console.log("DATA Y EL",data,this.el.components);
-     console.log("Botella 3d",this.el.components.botella);
+    // entity.parentNode.removeChild(entity);
+    // }
+  },
+ init: function() {
+   console.log("OBJETO",this.el);
+    var data = this.data
+    var el = this.el
+    this.el.addEventListener('click', () => {
+      let atributeAnimation = this.el.getAttribute('animation-mixer')
+      console.log(atributeAnimation)
+     if( atributeAnimation )
+     {
+      this.el.removeAttribute('animation-mixer')
+       
+      } else {
+        this.el.setAttribute('animation-mixer', {
+          clampWhenFinished: false,
+          clip: '*',
+          crossFadeDuration: 0,
+          duration: 0,
+          loop: 'repeat',
+          repetitions: 1,
+          timeScale: 1
+        })
+      }
+
+      console.log('click')
+    })
+
+    console.log('this.el.object3D', this.el.object3D)
+    console.log('DATA Y EL', data, this.el.components)
+    console.log('Botella 3d', this.el.components.botella)
     // this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
     // this.material = new THREE.MeshStandardMaterial({color: data.color});
-   // this.mesh = new THREE.Mesh( this.geometry, this.material );
-   // this.mesh.position.set(0,0,0)
+    // this.mesh = new THREE.Mesh( this.geometry, this.material );
+    // this.mesh.position.set(0,0,0)
     // el.setObject3D('mesh', this.mesh);
-  },
-   
+  }
 
   // randomDamage: function (min, max) {
   //          return Math.round(Math.random() * (max - min) + min);
@@ -63,4 +81,4 @@ AFRAME.registerComponent('botella', {
   //  updateHealthText: function () {
   //   document.querySelector('#text-health-'+this.data.id).setAttribute('text', 'value', 'HP ' + this.data.health);
   //  }
-});
+})
